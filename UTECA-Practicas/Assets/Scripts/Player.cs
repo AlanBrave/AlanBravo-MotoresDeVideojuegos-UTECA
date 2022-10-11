512d7e53010687fe9262575158c0dd702422e197
AlanBrave <alan.mr.yakult@gmail.com>
2022-10-11 10:34:21 -0500

    Parcial-1 Merge y scripting
    Se empezo a entender el uso de variables en un codigo de script de un player.

---
+++ UTECA-Practicas/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{   
+    public int edad = 25
+    public float velocidad - 4.0f;
+    public bool hasPowers - flase;
+    string name _ "Alan";
+    Sprite textura;
+    Player intancia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        name = name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
