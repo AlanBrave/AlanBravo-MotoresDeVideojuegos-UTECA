269a46aa5e1952b1e2c9f15ad4be80523cb99963
AlanBrave <alan.mr.yakult@gmail.com>
2022-11-29 08:49:53 -0600

    Parcial2 Ejercicios
    Uso del AudioManager

---
+++ UTECA-Practicas/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AudioClips_Soundtrack
+{
+    NONE,
+    SONG_1,
+    SONG_2,
+}
+
+public enum Audioclips_SFX
+{
+    NONE,
+    WALK,
+    PUNCH,
+    BULLET,
+}
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public AudioSource audioSouerce; //Background Music /
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
